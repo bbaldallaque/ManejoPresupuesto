2691f4c0e8060301776b2dd8ac283444cd3cdfe7
Bryant Baldallaque <38927423+bbaldallaque@users.noreply.github.com>
2021-12-06 23:48:11 -0400

    Tipos Cuentas
    Creamos el primer controlador de tipos cuentas

---
+++ Presupuesto/Presupuesto.Web/Controllers/TiposCuentasController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presupuesto.Web.Models;
+
+namespace Presupuesto.Web.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        public IActionResult Crear()
+        {
+          return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            return View();
+        }
+    }
+}
