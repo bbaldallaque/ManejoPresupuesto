983696fcb86e7275463f953367bfe17c1aaf9a37
Bryant Baldallaque <38927423+bbaldallaque@users.noreply.github.com>
2021-12-23 23:44:22 -0400

    Categoria
    Modulo de categoria

--- Presupuesto/Presupuesto.Web/Program.cs
+++ Presupuesto/Presupuesto.Web/Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<IRepositorioTiposCuentas, RepositorioTiposCuentas>();
 builder.Services.AddTransient<IServicioUsuarios, ServicioUsuarios>();
 builder.Services.AddTransient<IRepositorioCuenta, RepositorioCuenta>();
+builder.Services.AddTransient<IRepositorioCategoria, RepositorioCategoria>();  
 builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
---
+++ Presupuesto/Presupuesto.Web/Servicio/RepositorioCategoria.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Presupuesto.Web.Models;
+using System.Collections;
+
+namespace Presupuesto.Web.Servicio
+{
+    public interface IRepositorioCategoria
+    {
+		Task Actualizar(Categoria categoria);
+		Task Crear(Categoria categoria);
+        Task<IEnumerable<Categoria>> Obtner(int usuarioId);
+		Task<Categoria> ObtnerPorId(int id, int UsuarioId);
+	}
+
+    public class RepositorioCategoria : IRepositorioCategoria
+    {
+        private readonly string connectionString;
+
+        public RepositorioCategoria(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }                                                              
+
+        public async Task Crear(Categoria categoria)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(@"INSERT INTO Categorias (Nombre, TipoOperacionId, UsuarioId)
+                                                           Values (@Nombre, @TipoOperacionId, @UsuarioId); 
+                                                           SELECT SCOPE_IDENTITY();", categoria);
+            categoria.Id = id;  
+        }
+
+        public async Task<IEnumerable<Categoria>> Obtner(int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<Categoria>("SELECT * FROM Categorias WHERE UsuarioId = @UsuarioId", new { usuarioId });
+        }
+
+        public async Task<Categoria> ObtnerPorId(int id, int UsuarioId)
+		{
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryFirstOrDefaultAsync<Categoria>(
+                @"SELECT * FROM Categorias WHERE Id = @Id AND UsuarioId = @UsuarioId", new { id, UsuarioId});
+		}
+
+        public async Task Actualizar(Categoria categoria)
+		{
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"UPDATE Categorias SET Nombre = @Nombre, TipoOperacionId = @TipoOperacionId
+                                          WHERE Id = @Id", categoria);
+		}
+    }
+}
