078e4030f3735c4b174e613c39525a873601ba93
Bryant Baldallaque <38927423+bbaldallaque@users.noreply.github.com>
2022-01-02 16:13:05 -0400

    Autenticacion
    el usuario puede autenticarce, pudimos usar las cookies para la secciones

---
+++ Presupuesto/Presupuesto.Web/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presupuesto.Web.Models
+{
+	public class LoginViewModel
+	{
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El campo {0} debe de ser un correo electronico valido")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+		public bool Recuerdame { get; set; }
+	}
+}
---
+++ Presupuesto/Presupuesto.Web/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace Presupuesto.Web.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string EmailNormalizado { get; set; }
+
+        public string PasswordHash { get; set; }
+    }
+}
