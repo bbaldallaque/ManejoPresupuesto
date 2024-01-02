756c77c2a189fd62b579442c2b19777ceb8f3c13
Bryant Baldallaque <38927423+bbaldallaque@users.noreply.github.com>
2024-01-01 22:46:06 -0500

    Update RepositorioCuenta.cs

--- Presupuesto/Presupuesto.Web/Servicio/RepositorioCuenta.cs
+++ Presupuesto/Presupuesto.Web/Servicio/RepositorioCuenta.cs
@@ -28,6 +28,7 @@ namespace Presupuesto.Web.Servicio
             var id = await connection.QuerySingleAsync<int>(
                 @"INSERT INTO Cuentas (Nombre, TipoCuentaId, Descripcion, Balance)
                     VALUES (@Nombre, @TipoCuentaId, @Descripcion, @Balance);
+
                     SELECT SCOPE_IDENTITY();", cuenta);
 
             cuenta.Id = id;
